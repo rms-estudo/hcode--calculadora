b1e766a539f3599da5f21764a2a3878a59ff2363
Rodolfo Mariano <rodolfo.mariano@outlook.com>
2021-12-15 16:58:58 -0300

    feat: Add events onClick buttons

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,34 +1,84 @@
 class CalcController {
   constructor() {
     // this._  Indica atributo privado
-    this._displayCalc = '0'
+    this._calcDisplayEl = document.querySelector('#display')
+    this._calcDateEl = document.querySelector('#data')
+    this._calcTimeEl = document.querySelector('#hora')
+
+    this.locale = 'pt-BR'
     this._currentDate
     this.initialize()
+    this.initButtonsEvent()
   }
 
   get displayCalc() {
-    return this._displayCalc
+    return this._calcDisplayEl.innerHTML
   }
 
   set displayCalc(value) {
-    this._displayCalc = value
+    this._calcDisplayEl.innerHTML = value
   }
 
   get currentDate() {
-    return this._currentDate
+    return new Date()
   }
 
   set currentDate(value) {
     this._currentDate = value
   }
 
-  initialize() {
-    let calcDisplayEl = document.querySelector('#display')
-    let calcDateEl = document.querySelector('#data')
-    let calcTimeEl = document.querySelector('#hora')
+  get displayTime() {
+    return this._calcTimeEl.innerHTML
+  }
 
-    calcDisplayEl.innerHTML = '1234'
-    calcDateEl.innerHTML = '02/02/0220'
-    calcTimeEl.innerHTML = '17:00'
+  set displayTime(value) {
+    this._calcTimeEl.innerHTML = value
+  }
+
+  get displayDate() {
+    return this._calcDateEl.innerHTML
+  }
+
+  set displayDate(value) {
+    this._calcDateEl.innerHTML = value
+  }
+
+  setDisplayDateTime() {
+    this.displayDate = this.currentDate.toLocaleDateString(this.locale, {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric'
+    })
+    this.displayTime = this.currentDate.toLocaleTimeString(this.locale)
+  }
+
+  addEventListenerAll(element, events, fn) {
+    // separando os eventos
+    events.split(' ').forEach(event => {
+      element.addEventListener(event, fn, false)
+    })
+  }
+
+  initButtonsEvent() {
+    let buttons = document.querySelectorAll('#buttons > g, #parts > g')
+
+    buttons.forEach(button => {
+      this.addEventListenerAll(button, 'click drag', e => {
+        console.log(button.className.baseVal.replace('btn-', ''))
+      })
+
+      this.addEventListenerAll(button, 'mouseover mouseup mousedown', e => {
+        button.style.cursor = 'pointer'
+      })
+
+    })
+  }
+
+  initialize() {
+    this.setDisplayDateTime()
+
+    setInterval(() => {
+      this.setDisplayDateTime()
+    }, 1000)
   }
 }
